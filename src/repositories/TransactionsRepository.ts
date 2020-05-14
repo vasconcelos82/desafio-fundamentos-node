b13a22d7b71d8e180646902e5146b1ea5df8e5bc
vasconcelos82 <marquinhos.arv@gmail.com>
2020-05-14 14:21:15 -0300

    desafio-pronto-14maio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+enum Type {
+  Income = 'income',
+  Outcome = 'outcome',
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +25,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  private calculateBalance(operator: Type): number {
+    return this.transactions
+      .filter(t => t.type === operator)
+      .reduce((accum, curr) => accum + curr.value, 0);
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.calculateBalance(Type.Income);
+    const outcome = this.calculateBalance(Type.Outcome);
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const { total } = this.getBalance();
+    if (type === Type.Outcome && total < value) {
+      throw Error('Transaction is not allowed without a valid balance');
+    }
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
